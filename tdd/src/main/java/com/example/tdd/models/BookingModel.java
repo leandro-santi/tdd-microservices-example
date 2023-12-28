8dbaf8e30a2472bed26876c5e15501a322c21243
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-12-28 02:44:53 -0400

    Added basic tdd flow

--- tdd/src/main/java/com/example/tdd/models/BookingModel.java
+++ tdd/src/main/java/com/example/tdd/models/BookingModel.java
@@ -1,4 +1,4 @@
-package models;
+package com.example.tdd.models;
 
 import java.time.LocalDate;
 
--- tdd/src/test/java/com/example/tdd/BookingServiceTest.java
+++ tdd/src/test/java/com/example/tdd/BookingServiceTest.java
@@ -1,17 +1,48 @@
 package com.example.tdd;
 
-import org.junit.Assert;
+import com.example.tdd.models.BookingModel;
+import com.example.tdd.repository.BookingRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.tdd.service.BookingService;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 public class BookingServiceTest {
 
+    /*
+    TDD flow ->
+        Write a test
+        Fail this test
+        Fix the test
+        Repeat
+    */
+
+    @TestConfiguration
+    static class BookingServiceTestConfiguration {
+
+        @Bean
+        public BookingService bookingService() {
+            return new BookingService();
+        }
+
+    }
+
     @Autowired
     BookingService bookingService;
+
+    @MockBean
+    BookingRepository bookingRepository;
 
     @Test
     public void bookTestServiceDaysCalculator() {
@@ -19,6 +50,16 @@ public class BookingServiceTest {
         int days = bookingService.daysCalculatorWithDatabase(name);
 
         Assertions.assertEquals(days, 10);
+    }
+
+    @Before // Method is executed before anything
+    public void setup() {
+        LocalDate checkIn = LocalDate.parse("2023-12-20");
+        LocalDate checkOut = LocalDate.parse("2023-12-30");
+        BookingModel bookingModel = new BookingModel("1", "Leandro", checkIn, checkOut, 2);
+
+        Mockito.when(bookingRepository.findBookingModelByReserveName(bookingModel.getReserveName())).
+                thenReturn(Optional.of(bookingModel));
     }
 
 }
