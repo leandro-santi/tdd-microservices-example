70e66186e9ccbd517f1b6bf2e9c3af78cb117c8c
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-12-28 02:21:07 -0400

    Created initial project and first failed test

---
+++ tdd/src/test/java/com/example/tdd/BookingServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class BookingServiceTest {
+
+    @Autowired
+    BookingService bookingService;
+
+    @Test
+    public void bookTestServiceDaysCalculator() {
+        String name = "Leandro";
+        int days = bookingService.daysCalculatorWithDatabase(name);
+
+        Assertions.assertEquals(days, 10);
+    }
+
+}
